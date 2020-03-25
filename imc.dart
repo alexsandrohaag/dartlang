769b64e05154bd49a315cd3a7385f1e4074bf628
Alexsandro Haag <alex@hgsoft.com.br>
2020-03-25 18:43:08 -0300

    Algoritmo IMC

---
+++ imc.dart
@@ -0,0 +1,35 @@
+import "dart:io";
+
+main () {
+
+  /*
+  pegar o peso
+  pegar a altura
+  realizar o cálculo
+  retornar imc
+  */
+
+  print ("Qual seu peso? ");
+  var txtpeso = stdin.readLineSync();
+  print ("Qual sua altura? ");
+  var txtaltura = stdin.readLineSync();
+
+  var peso = int.parse(txtpeso);
+  var altura = double.parse(txtaltura);
+
+  var calculo = peso / (altura * altura);
+
+  if (calculo <= 18.5) {
+    print ("Abaixo do Peso");
+  } else if (calculo <= 24.9) {
+    print ("Peso Normal");
+  } else if (calculo <= 29.9) {
+    print ("Sobrepeso");
+  } else if (calculo <= 39.9) {
+    print ("Obesidade");
+  } else  {
+    print ("Obesidade Mórbida");
+  }
+
+  print (calculo);
+} 
